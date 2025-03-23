8ca41fe7531e1ef6a54925306d5505c902eebd16
Robson Issomoto <robson.issomoto@fatec.sp.gov.br>
2025-03-23 19:28:12 -0300

    Agenda05-DS-III-parte2 Validação Quantidade e Preço

--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -9,11 +9,50 @@ namespace MauiAppMinhasCompras.Models
 {
     public class Produto
     {
+        string _descricao;
+        double _quantidade, _preco;
+
+
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        public string Descricao { get; set; }
-        public double Quantidade { get; set; }
-        public double Preco { get; set; }
+        public string Descricao
+        {
+            get => _descricao;
+            set
+            {
+                if (value == null)
+                {
+                    throw new Exception("Por favor, preencha a descrição");
+                }
+
+                _descricao = value;
+            }
+        }
+        public double Quantidade
+        {
+            get => _quantidade;
+            set
+            {
+                if(value <= 0)
+                {
+                    throw new Exception("Por favor, informe quantidade maior que 0");
+                }
+
+                _quantidade = value;
+            }
+        }
+        public double Preco 
+        { 
+            get => _preco;
+            set
+            { 
+                if(value <= 0) 
+                {
+                    throw new Exception("Por favor, informe valor maior que R$0.00");
+                }
+                _preco = value;
+            } 
+        }
 
         public double Total { get => Quantidade * Preco; }
     }
